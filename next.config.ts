e396dbc4c0e0d0a2af1fd9f60a259731df97fa6f
Daran Soares <daran.explosivo.soares@gmail.com>
2025-09-11 07:34:49 -0300

    Adding Code to use as base

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    domains: ["images-assets.nasa.gov"],
+    path: "/_next/image",
+    loader: "default",
+  },
 };
 
 export default nextConfig;
---
+++ src/app/photo/index.js
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import {useState} from "react";
+
+import ImagePreview from '../components/ImagePreview'
+
+export default function Home({items}) {
+    const [search, setSearch] = useState("");
+    const [photos, setPhotos] = useState(items);
+    return (
+        <div className="container">
+            <Head>
+                <title>Nasa Image Index</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main className="main">
+                <h1 className="title">Welcome to the Index</h1>
+                {photos && photos.map((preview) => (<ImagePreview key={preview.data[0].nasa_id} thumbnailUrl={preview.links[0].href} nasaId={preview.data[0].nasa_id} />))}
+            </main>
+        </div>
+    );
+}
+
+export async function getStaticProps() {
+    const results = await fetch("https://images-api.nasa.gov/search?media_type=image");
+    const preview = await results.json();
+    const items = await preview.collection.items;
+    return {
+        props: {items},
+    };
+}
